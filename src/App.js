4e2f98d7a4dc505e07cb1d70c0296a294fc097d8
John G Mac2 <johngrantuk@googlemail.com>
2019-09-08 16:11:42 +0100

    First deploy.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [
+        {
+          name: 'GG',
+          message: 'This is getting too heavy'
+        },
+        {
+          name: 'JG',
+          message: 'This guy makes some decent points but is full of himself!!'
+        }
+      ]
+    };
+  }
+
+  render (){
+
+    let messages;
+    messages = this.state.messages.map(k => {
+        return <div>
+                  <h4>{k.name}</h4>
+                  <h5>{k.message}</h5>
+               </div>
+      });
+
+    return (
+      <div>
+        <h1>Gecko Chat</h1>
+        <h2>Page Name (Domain?)</h2>
+        <h3>List of users?</h3>
+
+        {messages}
+
+
+      </div>
+    )
+  }
 }
 
 export default App;
