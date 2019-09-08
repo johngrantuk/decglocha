82f4fb09d881e06265d6b6a8fc6c41a995ca9301
John G Mac2 <johngrantuk@googlemail.com>
2019-09-08 21:16:03 +0100

    Very basic functions working.

--- src/App.js
+++ src/App.js
@@ -9,17 +9,20 @@ class App extends React.Component {
       topic: 'Please load again',
       messages: [
         {
-          name: 'GG',
-          message: 'This is getting too heavy'
-        },
-        {
-          name: 'JG',
-          message: 'This guy makes some decent points but is full of himself!!'
+          author: "did:3:bafyreig725iek2y2segb2stom3s6tkfhv7kweneb4d4esa6omkkoi3ycom",
+          message: "Test Post",
+          postId: 'zdpuApB5LgEe8HPZHXTJ2wtihGgz21UXtvSbru3YaeL6ZNWCt',
+          timestamp: 1567973188
         }
       ]
     };
 
     this.auth3Box = this.auth3Box.bind(this);
+    this.boxSyncComplete = this.boxSyncComplete.bind(this);
+    this.Logout3Box = this.Logout3Box.bind(this);
+    this.openSpace = this.openSpace.bind(this);
+    this.spaceSyncComplete = this.spaceSyncComplete.bind(this);
+    this.addPost = this.addPost.bind(this);
   }
 
   componentDidMount() {
@@ -35,6 +38,49 @@ class App extends React.Component {
     // When you first authenticate the user's 3Box, all data might not be synced from the network yet.
     console.log('Oh yeah synchd for action');
     this.setState({isAuthenticated: true});
+    this.openSpace();
+  }
+
+  async spaceSyncComplete(){
+    // We advise against setting any data before this sync has happened.
+    console.log('Space Sync complete');
+    this.setState({isSpaceSync: true});
+  }
+
+  async openSpace(){
+
+    if(this.state.topic === 'Please load again'){
+      console.log('Check Topic URL!')
+    }
+
+    // If the user already has a space with this name, your application will gain access to it; if the user does not have the space, this method will automatically create one for them.
+    const space = await this.state.box.openSpace('decglocha', { onSyncDone: this.spaceSyncComplete });
+    console.log('Space opened.')
+    this.setState({space: space});
+
+    const thread = await this.state.space.joinThread(this.state.topic, {
+      firstModerator: '0x8f80708Cae88d8487A8A270E7a641f16cEEe472e',
+      members: false
+    })
+
+    console.log('Thread created: ' + this.state.topic);
+    this.setState({thread: thread});
+
+    this.GetPosts();
+  }
+
+  async addPost(){
+    var Post = "Test Post";
+    console.log('Adding Post: ' + Post)
+    await this.state.thread.post(Post);
+    this.GetPosts();
+  }
+
+  async GetPosts(){
+    const posts = await this.state.thread.getPosts();
+    console.log('Posts:');
+    console.log(posts);
+    this.setState({messages: posts});
   }
 
   async auth3Box(){
@@ -48,26 +94,41 @@ class App extends React.Component {
     box.onSyncDone(this.boxSyncComplete);       // When you first authenticate the user's 3Box, all data might not be synced from the network yet.
   }
 
-  render (){
+  async Logout3Box(){
+    // Closes 3Box session
+    this.state.box.logout();
+    this.setState({
+      isAuthenticated: false
+    })
+  }
 
+  render (){
+    let authenticate = <button className="btn btn-primary" onClick={this.auth3Box}>AUTH YOUR 3BOX</button>
     let messages;
+    let addPost;
     messages = this.state.messages.map(k => {
         return <div>
-                  <h4>{k.name}</h4>
+                  <h4>{k.timestamp}</h4>
+                  <h4>{k.author}</h4>
                   <h5>{k.message}</h5>
                </div>
       });
 
+    if(this.state.isAuthenticated){
+      authenticate = <button className="btn btn-primary" onClick={this.Logout3Box}>Logout 3Box</button>;
+      addPost = <button className="btn btn-primary" onClick={this.addPost}>Add Post</button>;
+    }
+
     return (
       <div>
         <h2>Page Name - {this.state.topic}</h2>
-        <h3>List of users?</h3>
+        <h3>List of users? {this.state.members}</h3>
 
-        <button className="btn btn-primary" onClick={this.auth3Box}>AUTH YOUR 3BOX</button>
+        {authenticate}
+        <p/>
 
         {messages}
-
-
+        {addPost}
       </div>
     )
   }
